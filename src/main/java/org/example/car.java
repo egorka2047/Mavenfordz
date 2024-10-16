1a162f583aef5f818ff7d14175492ef8fc85fdf2
Egor Shpadaruk <eshpadaruk3@gmail.com>
2024-10-16 05:13:35 +0300

    Updated Main & new file car.java

---
+++ src/main/java/org/example/car.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class car {
+
+    String name;
+    int speed;
+    String ownerName;
+
+    public car(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
+}
